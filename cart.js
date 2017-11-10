2eebca40c1bf28de02f8b6b8e4d90c9f93704df9
Scott Serage <xttocsurge51@gmail.com>
2017-11-09 20:04:14 -0700

    Completed step 4

--- cart.js
+++ cart.js
@@ -1,18 +1,21 @@
 const cars = require('./data/cars');
 
 module.exports = {
-  cart: null,
-  total: null,
+  cart: [],
+  total: 0,
 
-  addToCart: function() {
-
+  addToCart(car){
+    this.cart.push(car);
+    this.total += car.price;
   },
 
-  removeFromCart: function() {
-
+  removeFromCart(index, price){
+    this.cart.splice(index, 1);
+    this.total -= price;
   },
-  
-  checkout: function() {
 
+  checkout(){
+    this.cart = [];
+    this.total = 0;
   }
 };
--- cart.test.js
+++ cart.test.js
@@ -2,27 +2,30 @@ const cart = require('./cart')
     , cars = require('./data/cars');
 
 describe('Cart Properties: ', ()=>{
-  const {cart, total} = cart;
-
   test('Check if cart.cart is an array and is empty', ()=>{
-    expect(Array.isArray(cart)).toEqual(true);
-    expect(cart.length === 0).toEqual(true);
+    expect(Array.isArray(cart.cart)).toEqual(true);
+    expect(cart.cart.length === 0).toEqual(true);
   });
 
   test('Check if cart.total is number and zero', ()=>{
-    expect(total).toEqual(0);
+    expect(cart.total).toEqual(0);
   });
 });
 
 describe('Cart Methods: ',()=>{
+  afterEach(function() {
+    cart.cart = [];
+    cart.total = 0;
+  });
   test('Check if cart.addToCart increases the cart\'s cart.length each call and that it is adding to the end of cart.cart', ()=>{
     cart.addToCart(cars[3]);
     cart.addToCart(cars[4]);
     cart.addToCart(cars[6]);
-    expect(cart.length).toEqual(3);
+    console.log(cart.cart[0]);
+    expect(cart.cart.length).toEqual(3);
     expect(cart.cart[0]).toEqual(cars[3]);
-    expect(cart.cart[4]).toEqual(cars[4]);
-    expect(cart.cart[6]).toEqual(cars[6]);
+    expect(cart.cart[1]).toEqual(cars[4]);
+    expect(cart.cart[2]).toEqual(cars[6]);
   });
 
   test('Check if cart.addToCart increases the cart.total by the passed in car object\'s price on each call', ()=>{
